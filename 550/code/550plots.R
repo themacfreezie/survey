cbb72207ed9c9e211a8ea8ca464c143ef7d019a2
Brian E McGreal <bmcgreal@uw.edu>
2025-06-08 12:39:28 -0700

    final

--- 550/code/550plots.R
+++ 550/code/550plots.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 # load model objects
-ssm_chin <- readRDS(file=here::here("550", "data", "ssm_chin.rds"))
-ssm_coho <- readRDS(file=here::here("550", "data", "ssm_coho.rds"))
-ssm_stel <- readRDS(file=here::here("550", "data", "ssm_stel.rds"))
+ssm_chin <- readRDS(file=here::here("550", "data", "ssm_chinM15.rds"))
+ssm_coho <- readRDS(file=here::here("550", "data", "ssm_cohoM10.rds"))
+ssm_stel <- readRDS(file=here::here("550", "data", "ssm_stelM14.rds"))
 
 # modular code
 mod <- ssm_chin
@@ -34,8 +34,8 @@ data <- na.omit(data)
 
 # better plot
 ggplot(data=data, aes(x=R, y=a, color=Group)) + 
-  # geom_point(size=4, show.legend = NA) + 
-  geom_text(aes(label = method), size=8) + 
+  # geom_point(size=4, show.legend = NA) +
+  geom_text(aes(label = method), size=8) +
   labs(x = "variance",
        y='bias') +
   geom_hline(yintercept = 0, linetype = "dashed") +
@@ -44,11 +44,11 @@ ggplot(data=data, aes(x=R, y=a, color=Group)) +
 
 # better plot
 points <- ggplot(data=data, aes(x=R, y=a, color=Group)) + 
-  # geom_point(size=4, show.legend = NA) + 
-  geom_point(size=8) + 
-  labs(x = "variance",
+  geom_text(aes(label = method), size=8) +
+  # geom_point(size=8) + 
+  labs(x = "Variance",
        title='Chinook',
-       y='bias') +
+       y='Relative Bias') +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_color_manual(values = c("#c1a13c",
@@ -105,11 +105,11 @@ ggplot(data=data, aes(x=R, y=a, color=Group)) +
 
 # better plot
 points <- ggplot(data=data, aes(x=R, y=a, color=Group)) + 
-  # geom_point(size=4, show.legend = NA) + 
-  geom_point(size=8) + 
-  labs(x = "variance",
-       title='steelhead',
-       y='bias') +
+  geom_text(aes(label = method), size=8) +
+  # geom_point(size=8) + 
+  labs(x = "Variance",
+       title='Steelhead',
+       y='Relative Bias') +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_color_manual(values = c("#c1a13c",
@@ -166,11 +166,11 @@ ggplot(data=data, aes(x=R, y=a, color=Group)) +
 
 # better plot
 points <- ggplot(data=data, aes(x=R, y=a, color=Group)) + 
-  # geom_point(size=4, show.legend = NA) + 
-  geom_point(size=8) + 
-  labs(x = "variance",
-       title='coho',
-       y='bias') +
+  geom_text(aes(label = method), size=8) +
+  # geom_point(size=8) + 
+  labs(x = "Variance",
+       title='Coho',
+       y='Relative Bias') +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_color_manual(values = c("#c1a13c",
@@ -190,12 +190,99 @@ coho_data <- data
 coho_data$species <- "coho"
 
 # bind all data into one set
+ssm_chin <- readRDS(file=here::here("550", "data", "ssm_chinM11.rds"))
+ssm_coho <- readRDS(file=here::here("550", "data", "ssm_cohoM11.rds"))
+ssm_stel <- readRDS(file=here::here("550", "data", "ssm_stelM11.rds"))
+
+# modular code
+mod <- ssm_chin
+
+# grab parameter estimates for a & r
+A.data <- as.data.frame(mod$par$A)
+A.data <- tibble::rownames_to_column(A.data, "method")
+A.data$method <- substr(A.data$method, 2, nchar(A.data$method))
+
+R.data <- as.data.frame(mod$par$R)
+R.data <- tibble::rownames_to_column(R.data, "method")
+R.data$method <- substr(R.data$method, 2, nchar(R.data$method))
+
+data <- merge(R.data, A.data, by = "method", all.x = TRUE, all.y = TRUE)
+colnames(data) <- c("method","R", "a")
+data[is.na(data)] <- 0
+
+# pull in legend
+legend <- read_excel(here("550", "data", "method_key.xlsx"), col_names = TRUE)
+legend$method <- legend$Method
+legend <- legend[-c(1,2)]
+
+data <- merge(data, legend, by = "method", all.x = TRUE, all.y = TRUE)
+data <- na.omit(data)
+
+data$species <- 'Chinook'
+chin_data <- data
+
+# modular code
+mod <- ssm_coho
+
+# grab parameter estimates for a & r
+A.data <- as.data.frame(mod$par$A)
+A.data <- tibble::rownames_to_column(A.data, "method")
+A.data$method <- substr(A.data$method, 2, nchar(A.data$method))
+
+R.data <- as.data.frame(mod$par$R)
+R.data <- tibble::rownames_to_column(R.data, "method")
+R.data$method <- substr(R.data$method, 2, nchar(R.data$method))
+
+data <- merge(R.data, A.data, by = "method", all.x = TRUE, all.y = TRUE)
+colnames(data) <- c("method","R", "a")
+data[is.na(data)] <- 0
+
+# pull in legend
+legend <- read_excel(here("550", "data", "method_key.xlsx"), col_names = TRUE)
+legend$method <- legend$Method
+legend <- legend[-c(1,2)]
+
+data <- merge(data, legend, by = "method", all.x = TRUE, all.y = TRUE)
+data <- na.omit(data)
+
+data$species <- "Coho"
+coho_data <- data
+
+# modular code
+mod <- ssm_stel
+
+# grab parameter estimates for a & r
+A.data <- as.data.frame(mod$par$A)
+A.data <- tibble::rownames_to_column(A.data, "method")
+A.data$method <- substr(A.data$method, 2, nchar(A.data$method))
+
+R.data <- as.data.frame(mod$par$R)
+R.data <- tibble::rownames_to_column(R.data, "method")
+R.data$method <- substr(R.data$method, 2, nchar(R.data$method))
+
+data <- merge(R.data, A.data, by = "method", all.x = TRUE, all.y = TRUE)
+colnames(data) <- c("method","R", "a")
+data[is.na(data)] <- 0
+
+# pull in legend
+legend <- read_excel(here("550", "data", "method_key.xlsx"), col_names = TRUE)
+legend$method <- legend$Method
+legend <- legend[-c(1,2)]
+
+data <- merge(data, legend, by = "method", all.x = TRUE, all.y = TRUE)
+data <- na.omit(data)
+
+data$species <- "Steelhead"
+stel_data <- data
+
 data <- dplyr::bind_rows(chin_data, coho_data, stel_data)
 
 points <- ggplot(data=data, aes(x=R, y=a, color=Group, shape = species)) + 
-  geom_point(data=data, size=8) + 
-  labs(x = "variance",
-       y='bias') +
+  geom_text(aes(label = method), size=8) +
+  # geom_point(data=data, size=8) +
+  labs(x = "Variance",
+       title='All species',
+       y='Relative Bias') +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_color_manual(values = c("#c1a13c",
