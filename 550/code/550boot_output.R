08ea74f90364204c026a07fcf79ba6ff630c9b05
Brian E McGreal <bmcgreal@uw.edu>
2025-10-23 12:32:08 -0700

    goober peas

--- 550/code/550boot_output.R
+++ 550/code/550boot_output.R
@@ -1,8 +1,46 @@
-boot_chinM15 <- readRDS("~/dissertation/survey/github/550/data/boot_stelM14.rds")
-View(boot_chinM15)
-boot_chinM15$boot.params
+library(here)
+
+boot_chinM15 <- readRDS(here("550", "data", "boot_chinM15.rds"))
 df <- boot_chinM15$boot.params
 wdf <- data.frame(t(df))
 wdf <- wdf[, -c(18:36)]
-derp <- boxplot(wdf[,1:17])
-derp + abline(h = 0, col = "red")
+wdf_a <- wdf[, -c(9:17)]
+wdf_r <- wdf[, -c(1:8)]
+
+chin_a <- boxplot(wdf_a, range = 1.58)
+abline(h = 0, col = "red")
+title('Bias (Chinook)')
+
+chin_r <- boxplot(wdf_r, range = 1.58)
+abline(h = 0, col = "red")
+title('Variance (Chinook)')
+
+boot_cohoM10 <- readRDS(here("550", "data", "boot_cohoM10.rds"))
+df <- boot_cohoM10$boot.params
+wdf <- data.frame(t(df))
+wdf <- wdf[, -c(22:49)]
+wdf_a <- wdf[, -c(11:21)]
+wdf_r <- wdf[, -c(1:10)]
+
+coho_a <- boxplot(wdf_a, range = 1.58)
+abline(h = 0, col = "red")
+title('Bias (coho)')
+
+coho_r <- boxplot(wdf_r, range = 1.58)
+abline(h = 0, col = "red")
+title('Variance (coho)')
+
+boot_stelM14 <- readRDS(here("550", "data", "boot_stelM14.rds"))
+df <- boot_stelM14$boot.params
+wdf <- data.frame(t(df))
+wdf <- wdf[, -c(18:41)]
+wdf_a <- wdf[, -c(9:17)]
+wdf_r <- wdf[, -c(1:8)]
+
+stel_a <- boxplot(wdf_a, range = 1.58)
+abline(h = 0, col = "red")
+title('Bias (Steelhead)')
+
+stel_r <- boxplot(wdf_r, range = 1.58)
+abline(h = 0, col = "red")
+title('Variance (Steelhead)')
