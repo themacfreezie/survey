6c0e846f67b820743c0d04126f66c33bb3ca67ae
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2026-04-22 14:15:52 -0700

    fixing commit issue

--- code/primary/05-ARoutput.R
+++ code/primary/05-ARoutput.R
@@ -1,10 +1,8 @@
 ## SET WORKING DIR & PACKAGES
 library(here)
-library(MARSS)
-library(panelr)
 library(tidyverse)
 
-here::i_am("code/primary/04-bootstrap.R")
+here::i_am("code/primary/05-ARoutput.R")
 options(max.print=2000)
 
 # pull in data
@@ -16,6 +14,8 @@ load(here("data", "clean", "nosa_codes.Rda"))
 nosa <- merge
 
 load(here("data", "clean", "populations_list.Rda"))
+
+nmfs_popid <- read_excel(here("data", "raw", "cap-hli.xls"), sheet = "NOSA")
 
 # pull a & r estimates from ssms
 # modular code
@@ -85,3 +85,59 @@ merged_chin <- merge(nosa, chin_ARdata, by = c("MethodNameID", "CommonName"))
 merged_coho <- merge(nosa, coho_ARdata, by = c("MethodNameID", "CommonName"))
 merged_stel <- merge(nosa, stel_ARdata, by = c("MethodNameID", "CommonName"))
   # 173 obs less than nosa (due to dropped observations with little-used methods)
+
+merged_ARdata <- bind_rows(merged_chin, merged_coho, merged_stel)
+
+# collapse around population
+collapsed_ARdata <- merged_ARdata %>%
+  group_by(PopID, CommonName) %>%
+  summarize(mean_lnnosa = mean(lnnosa, na.rm = TRUE), 
+            mean_a = mean(a, na.rm = TRUE), 
+            mean_R = mean(R, na.rm = TRUE))
+
+# merge in population data
+popavgAR <- merge(collapsed_ARdata, pop_list, by = c("PopID"))
+  # why are there more lines than in collapsed_ARdata
+
+# steelhead
+length(unique(collapsed_ARdata$PopID))
+length(unique(pop_list$PopID))
+  # 3 doubles?
+table(pop_list$PopID)
+  # 239, 290, 503
+
+# try this again
+pop_list <- pop_list[-c(52, 68, 69), ]
+popavgAR <- merge(collapsed_ARdata, pop_list, by = c("PopID", "CommonName"))
+  # matched up
+
+# bring in nmfs popid
+nmfs_popid <- nmfs_popid[-c(1:4, 7:112)]
+
+lookup_nmfs <- nmfs_popid %>%
+  distinct(POPID, NMFS_POPID)
+
+length(unique(lookup_nmfs$POPID))
+table(lookup_nmfs$POPID)
+
+length(unique(lookup_nmfs$NMFS_POPID))
+table(lookup_nmfs$NMFS_POPID)
+  # popid 500 to 506 has na for NMFS popID 
+    # 3 steelhead and 3 chinook pops - john day and the lower columbia
+
+lookup_nmfs <- lookup_nmfs %>%
+  rename(PopID = POPID,
+         NWFSC_POP_ID = NMFS_POPID)
+
+# merge in NWFSC pop IDs (to match with gis)
+popavgAR <- left_join(popavgAR, lookup_nmfs, by = "PopID")
+
+# different species
+popavgAR_chin <- popavgAR %>% filter(CommonName=="Chinook Salmon")
+popavgAR_coho <- popavgAR %>% filter(CommonName=="Coho Salmon")
+popavgAR_stel <- popavgAR %>% filter(CommonName=="Steelhead")
+
+# save it up
+saveRDS(popavgAR_chin, file=here::here("data", "clean", "popavgAR_chin.rds"))
+saveRDS(popavgAR_coho, file=here::here("data", "clean", "popavgAR_coho.rds"))
+saveRDS(popavgAR_stel, file=here::here("data", "clean", "popavgAR_stel.rds"))
---
+++ code/primary/06-ARmap.R
@@ -0,0 +1,101 @@
+## SET WORKING DIR & PACKAGES
+library(ggspatial)
+library(here)
+library(prettymapr)
+library(sf)
+library(tidyverse)
+
+here::i_am("code/primary/06-ARmap.R")
+options(max.print=2000)
+
+# pull in AR data
+ARchin <- readRDS(here("data", "clean", "popavgAR_chin.rds"))
+ARcoho <- readRDS(here("data", "clean", "popavgAR_coho.rds"))
+ARstel <- readRDS(here("data", "clean", "popavgAR_stel.rds"))
+
+# pull in spatial layers
+gdb_path <- here("data", "raw", "WCR_Salmon_Steelhead_gdb_2015", "WCR_Salmon_Steelhead_gdb_2015.gdb")
+st_layers(gdb_path)
+  # I want 'fish' datalayer
+sf_fish <- read_sf(dsn = gdb_path, layer = "fish")
+
+# coho test case - as no NWFSC ids are missing
+sf_fish_combined <- sf_fish %>%
+  left_join(ARcoho, by = "NWFSC_POP_ID")
+
+sf_coho <- sf_fish_combined %>%
+  filter(!is.na(mean_a))
+
+# test plot
+sf_coho_nad83 <- st_transform(sf_coho, crs = 4269)
+
+ggplot(data = sf_coho_nad83) +
+  annotation_map_tile(type = "osm", zoom = 10) + 
+  geom_sf(aes(fill = mean_a), alpha = 0.7) + 
+  coord_sf(crs = 4269) + 
+  scale_fill_viridis_c(option = "plasma") + # Provides a high-contrast, accessible gradient
+  labs(title = "Map of bias in coho populations",
+       fill = "Mean bias") +
+  theme_minimal()
+
+ggplot(data = sf_coho_nad83) +
+  annotation_map_tile(type = "osm", zoom = 10) + 
+  geom_sf(aes(fill = mean_R), alpha = 0.7) + 
+  coord_sf(crs = 4269) + 
+  scale_fill_viridis_c(option = "plasma") + # Provides a high-contrast, accessible gradient
+  labs(title = "Map of precision in coho populations",
+       fill = "Mean variance") +
+  theme_minimal()
+
+# can we make these contiguous?
+contiguity_test <- sf_coho_nad83 %>%
+  group_by(NWFSC_POP_ID) %>%
+  summarize(geometry = st_union(SHAPE)) %>%
+  mutate(
+    # break multipolygons into individual polygons and count them
+    piece_count = lengths(st_cast(geometry, "POLYGON", warn = FALSE)),
+    is_contiguous = piece_count == 1
+  )
+
+# view groups that are NOT contiguous
+non_contiguous <- filter(contiguity_test, !is_contiguous)
+print(non_contiguous)
+  # it looks as though they are all contiguous..
+
+# can this be collapsed?
+sf_coho_nad83col <- sf_coho_nad83 %>%
+  group_by(NWFSC_POP_ID) %>%
+  summarize(
+    mean_lnnosa = mean(mean_lnnosa, na.rm = TRUE),
+    mean_a      = mean(mean_a, na.rm = TRUE),
+    mean_R      = mean(mean_R, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# replotting
+ggplot(data = sf_coho_nad83col) +
+  annotation_map_tile(type = "osm", zoom = 10) + 
+  geom_sf(aes(fill = mean_a), alpha = 0.7) + 
+  coord_sf(crs = 4269) + 
+  scale_fill_viridis_c(option = "plasma") + # Provides a high-contrast, accessible gradient
+  labs(title = "Map of relative bias in coho populations",
+       fill = "Mean relative bias") +
+  theme_minimal()
+
+ggplot(data = sf_coho_nad83col) +
+  annotation_map_tile(type = "osm", zoom = 10) + 
+  geom_sf(aes(fill = mean_R), alpha = 0.7) + 
+  coord_sf(crs = 4269) + 
+  scale_fill_viridis_c(option = "plasma") + # Provides a high-contrast, accessible gradient
+  labs(title = "Map of precision in coho populations",
+       fill = "Mean variance") +
+  theme_minimal()
+
+ggplot(data = sf_coho_nad83col) +
+  annotation_map_tile(type = "osm", zoom = 10) + 
+  geom_sf(aes(fill = mean_lnnosa), alpha = 0.7) + 
+  coord_sf(crs = 4269) + 
+  scale_fill_viridis_c(option = "plasma") + # Provides a high-contrast, accessible gradient
+  labs(title = "Map of population size in coho populations",
+       fill = "Average pop size (1980-2024)") +
+  theme_minimal()
